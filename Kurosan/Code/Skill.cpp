98ce9799b1b80be54a81dcdf86f4e2914046b8eb
ataru39 <122337099+ataru39@users.noreply.github.com>
2024-04-19 15:12:50 +0900

    スキル

---
+++ Kurosan/Code/Skill.cpp
@@ -0,0 +1,36 @@
+#include "Skill.h"
+#include "Dxlib.h"
+
+Skill::Skill()
+{
+}
+
+Skill::~Skill()
+{
+}
+
+void Skill::Initialize()
+{
+}
+
+void Skill::Update()
+{
+}
+
+void Skill::Draw()
+{
+}
+
+void Skill::Finalize()
+{
+}
+
+Vector2D Skill::GetLocation() const
+{
+	return Vector2D();
+}
+
+Vector2D Skill::GetBoxSize() const
+{
+	return Vector2D();
+}
--- Kurosan/Code/Skill.h
+++ Kurosan/Code/Skill.h
@@ -1,3 +1,25 @@
 #pragma once
 
 #include"../Utility/Vector2D.h"
+
+class Skill
+{
+private:
+	int image;			// 画像
+	Vector2D location;	// 位置座標
+	Vector2D box_size;	// 当たり判定の大きさ
+	Vector2D spawn_location;  // スポーン位置
+
+public:
+	Skill();
+	~Skill();
+
+	void Initialize();	// 初期化処理
+	void Update();		// 更新処理
+	void Draw();		// 描画処理
+	void Finalize();	// 終了時処理
+
+	Vector2D GetLocation() const;  //位置座標取得
+	Vector2D GetBoxSize()const;    //当たり判定の大きさ取得
+
+};
