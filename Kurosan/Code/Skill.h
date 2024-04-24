1efc75cb367ffad545720adb3ac3a6d463a3a212
ataru39 <122337099+ataru39@users.noreply.github.com>
2024-04-24 10:50:53 +0900

    skill

--- Kurosan/Code/Skill.h
+++ Kurosan/Code/Skill.h
@@ -7,8 +7,8 @@ class Skill
 protected:
 	int image;			// 画像
 	Vector2D location;	// 位置座標
+	Vector2D p_location;// プレイヤーの座標
 	Vector2D box_size;	// 当たり判定の大きさ
-	Vector2D spawn_location;  // スポーン位置
 	float speed;		// スキルの速度
 	float cool_time;	// スキルクールタイム
 	float damage;		// ダメージ
@@ -25,5 +25,5 @@ public:
 	Vector2D GetLocation() const;   //位置座標取得
 	Vector2D GetBoxSize() const;    //当たり判定の大きさ取得
 	Vector2D GetCoolTime() const;   //当たり判定の大きさ取得
-	Vector2D GetDamageTime() const; //当たり判定の大きさ取得
+	Vector2D GetDamage() const;		//当たり判定の大きさ取得
 };
